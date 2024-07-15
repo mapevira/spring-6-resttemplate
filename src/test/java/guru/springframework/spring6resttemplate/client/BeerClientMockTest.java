debed6a49194ebd0c34dfc0b152708f676f3e9d4
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-07-15 13:06:29 +0200

    212. Mock Test Query Param

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -72,6 +72,27 @@ class BeerClientMockTest {
         dtoJson = objectMapper.writeValueAsString(dto);
     }
 
+
+    @Test
+    void testListBeersWithQueryParam() throws JsonProcessingException {
+        String response = objectMapper.writeValueAsString(getPage());
+
+        URI uri = UriComponentsBuilder.fromHttpUrl(URL + BeerClientImpl.GET_BEER_PATH)
+                .queryParam("beerName", "ALE")
+                .build().toUri();
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestTo(uri))
+                .andExpect(queryParam("beerName", "ALE"))
+                .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
+
+        Page<BeerDTO> responsePage = beerClient
+                .listBeers("ALE", null, null, null, null);
+
+        assertThat(responsePage.getContent().size()).isEqualTo(1);
+    }
+
+
     @Test
     void testDeleteNotFound() {
         server.expect(method(HttpMethod.DELETE))
