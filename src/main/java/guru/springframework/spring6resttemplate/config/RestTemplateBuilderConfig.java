c43332f7e6573e435fa6c7df416dd94bf8adb902
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-11 18:49:59 +0200

    248. Mock Oauth2 Manager

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -27,12 +27,9 @@ public class RestTemplateBuilderConfig {
     @Value("${rest.template.rootUrl:http://localhost:8080}")
     private String rootUrl;
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
-
-    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
-
     @Bean
-    OAuth2AuthorizedClientManager auth2AuthorizedClientManager(){
+    OAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                               OAuth2AuthorizedClientService oAuth2AuthorizedClientService ){
         var authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
                 .clientCredentials()
                 .build();
@@ -46,11 +43,13 @@ public class RestTemplateBuilderConfig {
     }
 
     @Bean
-    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer,
+                                            OAuthClientInterceptor interceptor){
 
         assert rootUrl != null;
 
         return configurer.configure(new RestTemplateBuilder())
+                .additionalInterceptors(interceptor)
                 .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
     }
 
